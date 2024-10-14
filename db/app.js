983258805edbec3c46c14313728b2a10ea86a406
UArukonda <uarukonda@gmail.com>
2024-10-14 13:45:58 +0100

    add middleware for endpoint/api/topics and

--- db/app.js
+++ db/app.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const { getTopics } = require("./controllers/topics.controller.js");
 const app = express();
 
 app.use(express.json());
+
+//middleware
+app.get("/api/topics", getTopics);
+
+//last end point
+app.all("*", (request, response, next) => {
+  response.status(404).send({ msg: "Not Found" });
+});
+
+//error handling
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: "Internal Server Error" });
+});
 
 module.exports = app;
