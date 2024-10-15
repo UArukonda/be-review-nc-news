8b9c2adb96f762af4da5f6652fc8ded57d0f342f
UArukonda <uarukonda@gmail.com>
2024-10-15 17:05:57 +0100

    rename controller and model

---
+++ db/models/model.js
@@ -0,0 +1,43 @@
+const db = require("../connection.js");
+
+function selectTopics() {
+  return db.query("SELECT * FROM topics").then((response) => response.rows);
+}
+
+function selectArticles(sort_by = "article_id", order = "ASC") {
+  //   console.log(sort_by);
+  //   const allowed_sorts = ["article_id", "created_at", "topic", "author"];
+  //   if (!allowed_sorts.includes(sort_by)) {
+  //     return Promise.reject({ status: 404, msg: "Invalid input" });
+  //   }
+  //   let orderstring = "";
+  //   if (order) {
+  //     orderstring = order.toUpperCase();
+  //   }
+  //   let query = "SELECT * FROM articles";
+
+  //   if (sort_by) {
+  //     query += `ORDER BY ${sort_by}`;
+  //   }
+
+  return db
+    .query(
+      `SELECT articles.author,articles.title,articles.article_id,articles.topic,articles.created_at,articles.votes, articles.article_img_url,CAST(COUNT(comments.body) AS INTEGER) AS comment_count FROM articles LEFT JOIN comments on articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC`
+    )
+    .then((response) => {
+      return response.rows;
+    });
+}
+
+function selectArticleById(id) {
+  return db
+    .query("SELECT * FROM articles WHERE article_id=$1", [id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Not Found" });
+      }
+      return rows;
+    });
+}
+
+module.exports = { selectTopics, selectArticleById, selectArticles };
