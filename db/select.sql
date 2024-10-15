0eb1861abe0e43870827fe1de65228aae4e1a1aa
UArukonda <uarukonda@gmail.com>
2024-10-15 17:07:57 +0100

    testing

--- db/select.sql
+++ db/select.sql
@@ -3,5 +3,9 @@
 \dt
 
 -- select * from articles
-SELECT * FROM articles ORDER BY created_at DESC;
-SELECT * FROM articles;
+-- SELECT * FROM articles ORDER BY created_at DESC;
+SELECT article_id FROM articles;
+
+SELECT articles.author,articles.title,articles.article_id,articles.topic,articles.created_at,articles.votes, articles.article_img_url,COUNT(comments.body) AS comment_count FROM articles LEFT JOIN comments on articles.article_id = comments.article_id GROUP BY articles.article_id;
+
+--
