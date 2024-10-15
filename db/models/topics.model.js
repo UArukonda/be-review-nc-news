6007301b1199b9617a064f643d87d588ecebefe8
UArukonda <uarukonda@gmail.com>
2024-10-15 11:00:55 +0100

    add model selectArticle

--- db/models/topics.model.js
+++ db/models/topics.model.js
@@ -4,4 +4,15 @@ function selectTopics() {
   return db.query("SELECT * FROM topics").then((response) => response.rows);
 }
 
-module.exports = { selectTopics };
+function selectArticle(id) {
+  return db
+    .query("SELECT * FROM articles WHERE article_id=$1", [id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Not Found" });
+      }
+      return rows;
+    });
+}
+
+module.exports = { selectTopics, selectArticle };
